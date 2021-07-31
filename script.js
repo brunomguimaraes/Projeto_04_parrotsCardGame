86499c9bff596f32240b04892f9f47bbdbafeb62
Pedro Mafra <pmafrarm@gmail.com>
2021-07-31 16:01:54 -0300

    Game Logic fixed

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
+// Cards Selection
+
 function comparador() { 
 	return Math.random() - 0.5; 
 }
-
 
 let numberOfCards;
 
@@ -26,53 +27,134 @@ for (let i = 0; i < imgsChosen.length; i++) {
     cards.innerHTML += `<div class="card"><div class="front-face face" onclick="turnCard(this)"><img src="imagens/front.png"></div><div class="back-face face"><img src="imagens/${imgsChosen[i]}.gif"></div></div>`;
 }
 
+// Cards Turning - Memory Game
+
 let turnedCards = [];
+let firstOfPair;
+let secondOfPair;
+let counter = 0;
+let teste1;
 
 function turnCard (element) {
-
+    
     if (turnedCards.length === 0 || turnedCards.length === 1) {
-
+        
         element.classList.add("turn-front-face");
         element.nextElementSibling.classList.add("turn-back-face");
 
         turnedCards = document.querySelectorAll(".turn-back-face img");
-        let firstOfPair = turnedCards[0].src;
 
-        if (turnedCards.length > 1) {
-            let secondOfPair = turnedCards[1].src;
-
-            if (firstOfPair === secondOfPair) {
-                console.log("YEAH!");
-
+        if (turnedCards.length === 2) {
+            
+            if (turnedCards[0].src === turnedCards[1].src) {
+    
+                let teste1 = element.nextElementSibling.firstChild.src;
+                let allImages = document.getElementsByTagName("img");
+                for(let i = 0; i < allImages.length; i++) {
+                    if (allImages[i].src === teste1) {
+                        allImages[i].parentNode.classList.replace("turn-back-face", "pair-back");
+                        allImages[i].parentNode.previousElementSibling.classList.replace("turn-front-face", "pair-front");
+                    }
+                }
+                
+                turnedCards = [];
+                
             } else {
+
                 setTimeout(turnCardBack, 1000);
+
             }
-        }
-        
 
+        } 
+            
     }
-
-
-}
-
+  
+} 
 
 function turnCardBack () {
 
     for (let i = 0; i < 2; i++) {
         let backFace = document.querySelector(".turn-back-face");
-        backFace.classList.remove("turn-back-face");
-        let frontFace = document.querySelector(".turn-front-face");
-        frontFace.classList.remove("turn-front-face");
+        if (backFace !== null) {
+            backFace.classList.remove("turn-back-face");
+            backFace.previousElementSibling.classList.remove("turn-front-face");
+        }
     }
+
+    turnedCards = [];
 }
 
 
+/*
+document.querySelector('[src*="https://www.google.com""]')
+ SUBSTITUIR O HTML NO DOM
 
 
+let turnedCards = [];
+let firstOfPair;
+let secondOfPair;
+let counter = 0;
+let teste1;
 
+function turnCard (element) {
+    
+    if (turnedCards.length === 0 || turnedCards.length === 1) {
+        
+        element.classList.add("turn-front-face");
+        element.nextElementSibling.classList.add("turn-back-face");
 
+        turnedCards = document.querySelectorAll(".turn-back-face:not(.pair) img");
 
+        if (turnedCards.length === 2) {
+            
+            if (turnedCards[0].src === turnedCards[1].src) {
+                //element.classList.add("pair");
+                //element.nextElementSibling.classList.add("pair");
+                let teste1 = element.nextElementSibling.firstChild.src;
+                let allImages = document.getElementsByTagName("img");
+                for(let i = 0; i < allImages.length; i++) {
+                    if (allImages[i].src === teste1) {
+                        allImages[i].parentNode.classList.add("pair");
+                        allImages[i].parentNode.previousElementSibling.classList.add("pair");
+                    }
+                }
+                
+                turnedCards = [];
+                
+            } else {
 
+                for (let i = 0; i < 2; i++) {
+                    let algo = document.querySelector(".pair")
+
+                    if(algo !== null){
+                        algo.classList.remove("pair");
+                        algo.nextElementSibling.classList.remove("pair");
+                    }
+                }
+
+                setTimeout(turnCardBack, 1000);
+
+            }
+
+        } 
+            
+    }
+  
+} 
+
+function turnCardBack () {
+
+    for (let i = 0; i < 2; i++) {
+        let backFace = document.querySelector(".turn-back-face:not(.pair)");
+        if (backFace !== null) {
+            backFace.classList.remove("turn-back-face");
+            backFace.previousElementSibling.classList.remove("turn-front-face");
+        }
+    }
+
+    turnedCards = [];
+}
+*/
 
 
 
