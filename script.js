a0cb22b78c43c51400af07b32347b4cb52bcd1e2
Pedro Mafra <pmafrarm@gmail.com>
2021-08-01 23:19:36 -0300

    Timer and restart button added

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ function comparador() {
 
 let numberOfCards;
 
-while ((numberOfCards % 2) !== 0 || (numberOfCards < 4) || (numberOfCards > 14)) {
+while (((numberOfCards % 2) !== 0) || (numberOfCards < 4) || (numberOfCards > 14)) {
     numberOfCards = Number(prompt("Com quantas cartas irá jogar?"));
 }
 
@@ -27,10 +27,45 @@ for (let i = 0; i < imgsChosen.length; i++) {
     cards.innerHTML += `<div class="card"><div class="front-face face" onclick="turnCard(this)"><img src="imagens/front.png"></div><div class="back-face face"><img src="imagens/${imgsChosen[i]}.gif"></div></div>`;
 }
 
+// Time counter
+
+let seconds = 1;
+function secondsCounter () {
+
+    let timeBox = document.querySelector(".seconds");
+    if (seconds > 9) {
+        timeBox.innerText = `${seconds}`;
+    } else {
+        timeBox.innerText = `0${seconds}`;
+    }
+    seconds++;
+
+    if (seconds === 60){
+        seconds = 0;
+    }
+}
+
+let sec = setInterval(secondsCounter, 1000);
+
+let minutes = 1;
+function minutesCounter () {
+
+    let timeBox = document.querySelector(".minutes");
+    if (minutes > 9) {
+        timeBox.innerText = `${minutes}:`;
+    } else {
+        timeBox.innerText = `0${minutes}:`;
+    }
+    minutes++;
+    
+}
+
+let min = setInterval(minutesCounter, 1000*60);
+
 // Cards Turning - Memory Game
 
 let turnedCards = [];
-let teste1;
+let imgChosen;
 let counter = 0;
 
 function turnCard (element) {
@@ -46,10 +81,10 @@ function turnCard (element) {
             
             if (turnedCards[0].src === turnedCards[1].src) {
     
-                let teste1 = element.nextElementSibling.firstChild.src;
+                let imgChosen = element.nextElementSibling.firstChild.src;
                 let allImages = document.getElementsByTagName("img");
                 for(let i = 0; i < allImages.length; i++) {
-                    if (allImages[i].src === teste1) {
+                    if (allImages[i].src === imgChosen) {
                         allImages[i].parentNode.classList.replace("turn-back-face", "pair-back");
                         allImages[i].parentNode.previousElementSibling.classList.replace("turn-front-face", "pair-front");
                     }
@@ -62,11 +97,8 @@ function turnCard (element) {
                 setTimeout(turnCardBack, 1000);
 
             }
-
-        } 
-            
+        }         
     }
-  
 } 
 
 function turnCardBack () {
@@ -78,93 +110,30 @@ function turnCardBack () {
             backFace.previousElementSibling.classList.remove("turn-front-face");
         }
     }
-
     turnedCards = [];
 }
+
+// Winning message
 
 function gameFinished () {
-    let testando = document.querySelectorAll(".pair-front");
-    if (testando.length === numberOfCards) {
-        alert(`Você ganhou em ${counter} jogadas!`);
-    }
-}
 
-
-/*
-document.querySelector('[src*="https://www.google.com""]')
- SUBSTITUIR O HTML NO DOM
-
-
-let turnedCards = [];
-let firstOfPair;
-let secondOfPair;
-let counter = 0;
-let teste1;
-
-function turnCard (element) {
-    
-    if (turnedCards.length === 0 || turnedCards.length === 1) {
-        
-        element.classList.add("turn-front-face");
-        element.nextElementSibling.classList.add("turn-back-face");
-
-        turnedCards = document.querySelectorAll(".turn-back-face:not(.pair) img");
-
-        if (turnedCards.length === 2) {
-            
-            if (turnedCards[0].src === turnedCards[1].src) {
-                //element.classList.add("pair");
-                //element.nextElementSibling.classList.add("pair");
-                let teste1 = element.nextElementSibling.firstChild.src;
-                let allImages = document.getElementsByTagName("img");
-                for(let i = 0; i < allImages.length; i++) {
-                    if (allImages[i].src === teste1) {
-                        allImages[i].parentNode.classList.add("pair");
-                        allImages[i].parentNode.previousElementSibling.classList.add("pair");
-                    }
-                }
-                
-                turnedCards = [];
-                
-            } else {
-
-                for (let i = 0; i < 2; i++) {
-                    let algo = document.querySelector(".pair")
-
-                    if(algo !== null){
-                        algo.classList.remove("pair");
-                        algo.nextElementSibling.classList.remove("pair");
-                    }
-                }
-
-                setTimeout(turnCardBack, 1000);
-
-            }
-
-        } 
-            
-    }
-  
-} 
-
-function turnCardBack () {
-
-    for (let i = 0; i < 2; i++) {
-        let backFace = document.querySelector(".turn-back-face:not(.pair)");
-        if (backFace !== null) {
-            backFace.classList.remove("turn-back-face");
-            backFace.previousElementSibling.classList.remove("turn-front-face");
+    let allHits = document.querySelectorAll(".pair-front");
+    if (allHits.length === numberOfCards) {
+        if (minutes === 1) {
+            alert(`Você ganhou em ${seconds - 1} segundo(s) com ${counter} jogadas!`);
+        } else {
+            alert(`Você ganhou em ${minutes - 1} minuto(s) e ${seconds - 1} segundo(s) com ${counter} jogadas!`);
         }
+        let restartButton = document.querySelector(".button-container");
+        restartButton.classList.remove("hide");
+        clearInterval(sec);
+        clearInterval(min);
     }
-
-    turnedCards = [];
 }
-*/
 
-
-
-
-
+function playAgain () {
+    location.reload();
+}
 
 
 
